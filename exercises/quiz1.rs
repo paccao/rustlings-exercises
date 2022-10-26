39097b686ea49e3197ac00ef9747d8450cc82df9
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-10-26 16:34:48 +0200

    complete first quiz, mut variable, function, types, omit semicolon

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples (apples:i32) -> i32 {
+    let mut apple_cost: i32 = 2;
+    if apples > 40 {
+        apple_cost = 1;
+        apples * apple_cost
+    } else {
+        apples * apple_cost
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
