26b0d454c736c6cdaf5141f487f0a034a60ddf3b
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-11-03 10:52:48 +0100

    add classic c struct and tuple struct

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -20,7 +20,11 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = struct Green {
+            red: 0,
+            green: 255,
+            blue: 0,
+        }
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -30,7 +34,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = struct Green(i32,i32,i32);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -40,7 +44,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        let unit_like_struct = 
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
